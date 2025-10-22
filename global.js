52b605e15996fee4b1a5885d65ae5ce8cb690718
zmosDS <122930289+zmosDS@users.noreply.github.com>
2025-10-22 15:36:38 -0700

    Update global.js

--- global.js
+++ global.js
@@ -139,3 +139,18 @@ for (const p of pages) {
   });
 })();
 
+ // Projects
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
