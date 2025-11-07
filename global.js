e10beefd5b8f9ef499fd432b8cfebe8abc461dd2
zmosDS <122930289+zmosDS@users.noreply.github.com>
2025-11-06 17:39:09 -0800

    update

--- global.js
+++ global.js
@@ -16,6 +16,7 @@ const pages = [
   { url: "projects/",   title: "Projects" },
   { url: "resume/",     title: "Resume" },
   { url: "contact/",    title: "Contact" },
+  { url: "meta/",    title: "Meta" },
   { url: "https://github.com/", title: "Github" },
 ];
 
