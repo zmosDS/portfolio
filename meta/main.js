dec252f090868e7016bfffe037ddd17c454c22ec
zmosDS <122930289+zmosDS@users.noreply.github.com>
2025-11-06 22:07:58 -0800

    update

---
+++ meta/main.js
@@ -0,0 +1,14 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+async function loadData() {
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+
+  return data;
+}
