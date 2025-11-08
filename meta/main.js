16c8d12fd831e4a0597064544b5b87d3efc5a802
zmosDS <122930289+zmosDS@users.noreply.github.com>
2025-11-08 00:59:32 -0800

    updates

--- meta/main.js
+++ meta/main.js
@@ -1,14 +1,104 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadData() {
-  const data = await d3.csv('loc.csv', (row) => ({
+  const data = await d3.csv("loc.csv", (row) => ({
     ...row,
-    line: Number(row.line), // or just +row.line
+    line: Number(row.line),
     depth: Number(row.depth),
     length: Number(row.length),
-    date: new Date(row.date + 'T00:00' + row.timezone),
+    date: new Date(row.date + "T00:00" + row.timezone),
     datetime: new Date(row.datetime),
   }));
-
   return data;
 }
+
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+
+      let ret = {
+        id: commit,
+        url: "https://github.com/vis-society/lab-7/commit/" + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, "lines", {
+        value: lines,
+        enumerable: false,
+        writable: true,
+        configurable: true,
+      });
+
+      return ret;
+    });
+}
+
+function renderCommitInfo(data, commits) {
+  const stats = [
+    { label: "COMMITS", value: commits.length },
+    { label: "FILES", value: d3.group(data, d => d.file).size },
+    { label: "TOTAL LOC", value: data.length },
+    { label: "MAX DEPTH", value: d3.max(data, d => d.depth) },
+    { label: "LONGEST LINE", value: d3.max(data, d => d.length) },
+    { label: "MAX LINES", value: d3.max(d3.groups(data, d => d.file).map(([f, lines]) => lines.length)) }
+  ];
+
+  const container = d3.select("#stats")
+    .append("div")
+    .attr("class", "summary-box");
+
+  const item = container.selectAll("div.stat")
+    .data(stats)
+    .enter()
+    .append("div")
+    .attr("class", "stat");
+
+  item.append("div").attr("class", "value").text(d => d.value);
+  item.append("div").attr("class", "label").text(d => d.label);
+}
+
+function renderScatterPlot(data, commits) {
+  // Put all the JS code of Steps inside this function
+  const width = 1000;
+  const height = 600;
+
+  const svg = d3
+  .select('#chart')
+  .append('svg')
+  .attr('viewBox', `0 0 ${width} ${height}`)
+  .style('overflow', 'visible');
+
+  const xScale = d3
+  .scaleTime()
+  .domain(d3.extent(commits, (d) => d.datetime))
+  .range([0, width])
+  .nice();
+
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+
+renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
